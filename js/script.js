4547d462077eec701d5a3864c86ccb3540175ad9
Ryanov Akbar <ryanovuid@gmail.com>
2024-03-30 14:11:31 +0700

    Portfolio Updated

--- js/script.js
+++ js/script.js
@@ -1,31 +1,57 @@
-// <!-- jQuery Smooth Scrolling Starts -->
-$('a[href*="#"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top,
-    }, 200, 'linear')
+// Navbar Fixed
+window.onscroll = () => {
+    const header = document.querySelector('header');
+    
+    if (window.scrollY > 0) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+/* */
+
+
+// Hamburger and Nav Menu
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
-// <!-- jQuery Smooth Scrolling Ends -->
 
 
-// <!-- jQuery Navbar Active Class Starts -->
-$('.nav-link').on('click', function () {
-    $('.nav-link').removeClass('active');
-    $(this).addClass('active');
+/* */
+
+
+// Get all anchor elements with href containing '#'
+let anchorLinks = document.querySelectorAll('a[href*="#"]');
+
+// Loop through each anchor element
+anchorLinks.forEach(function(link) {
+    // Add click event listener
+    link.addEventListener('click', function(e) {
+        // Prevent default behavior
+        e.preventDefault();
+        
+        // Get the target element's ID from href attribute
+        let targetId = this.getAttribute('href').substring(1);
+        
+        // Find the target element
+        let targetElement = document.getElementById(targetId);
+        
+        // Scroll to the target element
+        if (targetElement) {
+            // Get the distance of the target element from the top of the document
+            let offsetTop = targetElement.offsetTop;
+            
+            // Smooth scroll animation
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    });
 });
-// <!-- jQuery Navbar Active Class Ends -->
-
-
-// <!-- Typed js Effect Starts -->
-const typed = new Typed(".typing-text", {
-    strings: [
-        "Full Stack Web Developer",
-        "Mobile App Developer",
-    ],
-    loop: true,
-    startDelay: 300,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
-});
-// <!-- Typed js Effect Ends -->
